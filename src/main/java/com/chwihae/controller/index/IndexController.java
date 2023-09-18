b1590b9216e044ab4b8eb84d5189a7a635be84c1
dragonappear <dragonappear1@gmail.com>
2023-09-18 20:11:45 +0900

    initialize project

---
+++ src/main/java/com/chwihae/controller/index/IndexController.java
@@ -0,0 +1,14 @@
+package com.chwihae.controller.index;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping
+    public ResponseEntity<Void> index() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
