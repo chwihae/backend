9757ba21d5eb0160e360fb3bcc902c772aa6112a
dragonappear <dragonappear1@gmail.com>
2023-09-20 00:38:19 +0900

    Modify: kakao login handler

--- src/main/java/com/chwihae/client/kakao/KakaoTokenFeignClient.java
+++ src/main/java/com/chwihae/client/kakao/KakaoTokenFeignClient.java
@@ -1,11 +1,13 @@
 package com.chwihae.client.kakao;
 
 import com.chwihae.client.kakao.config.KakaoFeignConfig;
-import com.chwihae.client.kakao.request.KakaoTokenRequest;
 import com.chwihae.client.kakao.response.KakaoTokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @FeignClient(
         name = "kakao-token-client",
@@ -14,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 )
 public interface KakaoTokenFeignClient {
 
-    @PostMapping
-    KakaoTokenResponse requestToken(@RequestBody KakaoTokenRequest kakaoTokenRequest);
+    @PostMapping(consumes = APPLICATION_FORM_URLENCODED_VALUE,
+            produces = APPLICATION_JSON_VALUE)
+    KakaoTokenResponse requestToken(@RequestParam("grant_type") String grantType,
+                                    @RequestParam("client_id") String clientId,
+                                    @RequestParam("client_secret") String clientSecret,
+                                    @RequestParam("redirect_uri") String redirectUri,
+                                    @RequestParam("code") String code);
 }
