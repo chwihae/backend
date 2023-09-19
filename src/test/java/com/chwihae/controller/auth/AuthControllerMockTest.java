11a7b85d9d0e7410ba51e89c4f9ee596ecd3e336
dragonappear <dragonappear1@gmail.com>
2023-09-20 00:15:53 +0900

    Add: auth controller test

---
+++ src/test/java/com/chwihae/controller/auth/AuthControllerMockTest.java
@@ -0,0 +1,77 @@
+package com.chwihae.controller.auth;
+
+import com.chwihae.controller.ApiResponse;
+import com.chwihae.dto.auth.request.KakaoLoginRequest;
+import com.chwihae.dto.auth.response.LoginResponse;
+import com.chwihae.exception.CustomException;
+import com.chwihae.infra.MockTestSupport;
+import com.chwihae.service.auth.AuthService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static com.chwihae.exception.CustomExceptionError.INVALID_KAKAO_AUTHORIZATION_CODE;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@MockTestSupport
+class AuthControllerMockTest {
+
+    @InjectMocks
+    AuthController authController;
+
+    @Mock
+    AuthService authService;
+
+    @Test
+    @DisplayName("POST /api/v1/auth/kakao-login - 성공")
+    void kakaoLoginSuccessTest() throws Exception {
+        //given
+        KakaoLoginRequest request = KakaoLoginRequest.builder()
+                .authorizationCode("authorization code")
+                .redirectionUri("redirection uri")
+                .build();
+
+        Long userId = 1L;
+        String token = "token";
+        String refreshToken = "refresh token";
+
+        LoginResponse loginResponse = LoginResponse.builder()
+                .userId(userId)
+                .token(token)
+                .refreshToken(refreshToken)
+                .build();
+
+        given(authService.kakaoLogin(anyString(), anyString()))
+                .willReturn(loginResponse);
+
+        //when
+        ApiResponse<LoginResponse> response = authController.kakaoLogin(request);
+
+        //then
+        Assertions.assertThat(response.getData())
+                .extracting("userId", "token", "refreshToken")
+                .containsExactly(userId, token, refreshToken);
+    }
+
+    @Test
+    @DisplayName("POST /api/v1/auth/kakao-login - 실패 (유효하지 않은 인가 코드)")
+    void kakaoLoginFailWithInvalidAuthorizationCodeTest() throws Exception {
+        //given
+        KakaoLoginRequest request = KakaoLoginRequest.builder()
+                .authorizationCode("authorization code")
+                .redirectionUri("redirection uri")
+                .build();
+
+        given(authService.kakaoLogin(anyString(), anyString()))
+                .willThrow(new CustomException(INVALID_KAKAO_AUTHORIZATION_CODE));
+
+        //when //then
+        Assertions.assertThatThrownBy(() -> authController.kakaoLogin(request))
+                .isInstanceOf(CustomException.class)
+                .extracting("error")
+                .isEqualTo(INVALID_KAKAO_AUTHORIZATION_CODE);
+    }
+}
