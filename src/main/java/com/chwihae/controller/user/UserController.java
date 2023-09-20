57d36972eeb60ef5d72e1d73b05c3fffb1887e12
dragonappear <dragonappear1@gmail.com>
2023-09-20 18:27:54 +0900

    Modify: add api index.html

--- src/main/java/com/chwihae/controller/user/UserController.java
+++ src/main/java/com/chwihae/controller/user/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    // TODO
     @GetMapping("/statistics")
     public ApiResponse<UserStatisticsResponse> getUserStatistics(@CurrentUser UserContext userContext) {
         return ApiResponse.ok(UserStatisticsResponse.of(0, 0));
